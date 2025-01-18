f299f9c14b0a725325ae4cabd2f50f34ede3fc92
EulaFRL <97711833+EulaFRL@users.noreply.github.com>
2025-01-18 17:56:01 +0800

    filename format debug

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ LLAMA_KEY = ""
 
 GROUND_TRUTH_DIRS_DICT = dict()
 CITY_IN_FORMAT_DICT = {"boston":"name_cleaned_parsed_boston.granicus.com_player_clip_meeting_code_view_id=1_redirect=true.json", 
-                     "cabq":"name_cleaned_parsed_cabq.granicus.com_player_clip_meeting_code_view_id=2_redirect=true", 
-                     "denver":"name_cleaned_parsed_denver.granicus.com_player_clip_meeting_code_view_id=180_redirect=true", 
-                     "johnson":"name_cleaned_parsed_johnson-county.granicus.com_player_clip_meeting_code_view_id=1_redirect=true", 
-                     "longbeach":"name_cleaned_parsed_longbeach.granicus.com_player_clip_meeting_code_view_id=84_redirect=true", 
-                     "milwaukee":"name_cleaned_parsed_milwaukee.granicus.com_player_clip_meeting_code_view_id=2_redirect=true"}
+                     "cabq":"name_cleaned_parsed_cabq.granicus.com_player_clip_meeting_code_view_id=2_redirect=true.json", 
+                     "denver":"name_cleaned_parsed_denver.granicus.com_player_clip_meeting_code_view_id=180_redirect=true.json", 
+                     "johnson":"name_cleaned_parsed_johnson-county.granicus.com_player_clip_meeting_code_view_id=1_redirect=true.json", 
+                     "longbeach":"name_cleaned_parsed_longbeach.granicus.com_player_clip_meeting_code_view_id=84_redirect=true.json", 
+                     "milwaukee":"name_cleaned_parsed_milwaukee.granicus.com_player_clip_meeting_code_view_id=2_redirect=true.json"}
